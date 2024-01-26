35831b9204e78e9ca09f52aab14a4f16f1791365
Voitila Araujo <voitilaaraujo@gmail.com>
2024-01-25 22:17:33 -0300

    add options

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -2,6 +2,23 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
+
 vim.g.mapleader = " "
+
 vim.opt.number = true
 vim.opt.relativenumber = true
+
+vim.opt.smartindent = true
+vim.opt.wrap = false
+
+vim.opt.hlsearch = true
+vim.opt.incsearch = true
+
+vim.opt.termguicolors = true
+
+vim.opt.scrolloff = 8
+vim.opt.updatetime = 50
+
+vim.opt.colorcolumn = "100"
+vim.opt.signcolumn = "yes"
+
